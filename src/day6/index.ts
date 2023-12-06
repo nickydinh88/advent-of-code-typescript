35c2d0a6e8ab6cf7a8aa7a45b2903569e47ea451
Nicky Dinh <thinhdinh@nvg.vn>
2023-12-06 12:06:14 +0700

    day 6

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,7 +1,31 @@
+// @ts-nocheck
 import * as path from "path";
 import { parseInput } from "../util";
 
-const input = parseInput(path.join(__dirname, "input.txt"));
+const input = parseInput(path.join(__dirname, "input.txt"), { split: { delimiter: '\n', mapper: false } });
+
+function calculateWaysToWin(time, distance) {
+  let ways = 0;
+  for (let i = 1; i < time; i++) {
+      if (i * (time - i) > distance) {
+          ways++;
+      }
+  }
+  return ways;
+}
+
+function calculateTotalWays(races) {
+  return races.reduce((total, race) => total * calculateWaysToWin(race.time, race.distance), 1);
+}
+
+let races = [
+  { time: 57, distance: 291 },
+  { time: 72, distance: 1172 },
+  { time: 69, distance: 1176 },
+  { time: 92, distance: 2026 }
+];
+
+console.log(calculateTotalWays(races)); // Outputs: 288
 
 function main() {
   console.log(input);
